a176a77b15498892186db6bc49c0c43ae101bdfa
David Deneroff <dgdeneroff@gmail.com>
2015-04-21 12:12:24 -0400

    Add proper tag format

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -46,7 +46,7 @@ class QuestionsController < ApplicationController
     end
 
     def render_page
-      STATSD.increment('web.page_views', :tags => ['support', page:questions])
+      STATSD.increment('web.page_views', :tags => ['support', 'page:questions'])
     end
 
 end
